ce2b9ed38f11b6cbe82427f8f467d0886953d7e7
natashagp <natasha.paulon@gmail.com>
2020-07-02 18:19:10 -0300

    app agenda de contatos com flutter

---
+++ android/app/src/main/kotlin/com/natashapaulon/agenda_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.natashapaulon.agenda_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:agenda_contatos/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Agenda de Contatos',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
